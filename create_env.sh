20384f92b3899c8272c3ddb64dc7e98d1da6a6b2
Jonas Jabari <jonas@basemate.com>
2020-09-22 23:03:54 +0200

    changed secrete key base approach due to errors

--- create_env.sh
+++ create_env.sh
@@ -1,5 +1,5 @@
 echo "" >> ./.env
-echo "RAILS_MASTER_KEY=$WEBSITE_RAILS_MASTER_KEY" >> ./.env
+echo "SECRET_KEY_BASE=$SECRET_KEY_BASE" >> ./.env
 echo "MAILER_ADDRESS=$MAILER_ADDRESS" >> ./.env
 echo "MAILER_DOMAIN=$MAILER_DOMAIN" >> ./.env
 echo "MAILER_PASSWORD=$MAILER_PASSWORD" >> ./.env
