de61fc671a6bf7ceeded198bf384bfef1e15587e
Jonas Jabari <jonas@basemate.com>
2020-09-29 17:58:20 +0200

    fixed secretes for dev, added dockerized dev workflow, added youtube intro video

--- app/matestack/website/components/features_section.rb
+++ app/matestack/website/components/features_section.rb
@@ -15,8 +15,14 @@ class Website::Components::FeaturesSection < Matestack::Ui::Component
             paragraph text: t("landing_page.features.#{feature_key}")[:text]
           end
         end
-
       end
+
+      div class: 'row mt-5 justify-content-center' do
+        div class: 'col col-md-6 pt-5 text-center' do
+          youtube height: "350px", width: "100%", youtube_id: '83AERu6pmg8', start_at: 0, privacy_mode: true
+        end
+      end
+      
     end
   end
 
