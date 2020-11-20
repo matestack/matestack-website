fe6a858ed27ad7a84c060d02796738fee6aa6b61
Jonas Jabari <jonas@basemate.com>
2020-11-20 19:08:24 +0100

    added missing mailto link

--- app/matestack/website/components/bootstrap_access_section.rb
+++ app/matestack/website/components/bootstrap_access_section.rb
@@ -7,7 +7,9 @@ class Website::Components::BootstrapAccessSection < Matestack::Ui::Component
           heading size: 3, text: t("addons.bootstrap.access.title")
           paragraph class: "mt-4", text: t("addons.bootstrap.access.text_1")
           paragraph text: t("addons.bootstrap.access.text_2")
-          button class: "btn btn-primary mt-2", text: t("addons.bootstrap.access.cta")
+          link path: "mailto:jonas@matestack.io" do
+            button class: "btn btn-primary mt-2", text: t("addons.bootstrap.access.cta")
+          end
         end
       end
     end
